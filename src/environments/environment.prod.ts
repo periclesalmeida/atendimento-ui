9904e811d1b49edac7b80a00cf3ce96de1b8d451
Péricles Almeida Jr <jose.pericles.almeida@gmail.com>
2020-03-07 22:05:37 +0000

    Setup circleci

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: false,
-  API_URL: '//10.1.16.37:4000',
-  tokenWhitelistedDomains: [  new RegExp('10.1.16.37:4000') ],
-//  tokenBlacklistedRoutes: [ ],
+  API_URL: 'http://localhost:8083',
+  tokenWhitelistedDomains: [  new RegExp('localhost:8083') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ],
   env: 'dev'
-  //tokenBlacklistedRoutes: [ new RegExp('localhost:?[0-9]*\/oauth\/token') ]
 };
